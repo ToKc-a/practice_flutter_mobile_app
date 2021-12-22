d8e18ccd9b46c3456d97949ddbc8f0d5d55fcf23
ToKc <ant.tokc@gmail.com>
2021-12-22 16:41:29 +0200

    add expanded_demo_screen

--- lib/expanded_demo_screen.dart
+++ lib/expanded_demo_screen.dart
@@ -7,7 +7,62 @@ class ExpandedDemoScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Row(children: [Text("Короткий текст")],
+        appBar: AppBar(),
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Column(
+            children: [
+              Row(
+                children: const [
+                  Expanded(
+                      child: Text(
+                    "Очень очень очень очень очень очень очень очень очень длинный текст",
+                    style: TextStyle(fontSize: 18),
+                  )),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      height: 100,
+                      color: Colors.green,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      height: 100,
+                      color: Colors.red,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      height: 100,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  const Expanded(
+                    flex: 2,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.account_box_rounded),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                      child: ElevatedButton(
+                          onPressed: () {}, child: Text("Регистрация")))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
