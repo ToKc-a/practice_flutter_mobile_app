9a00000d49fa2dd53d8610b5c2c16e7bc000eaa2
ToKc <ant.tokc@gmail.com>
2021-12-28 17:21:51 +0200

    add navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:practice_mobile_app/expanded_demo_screen.dart';
+import 'package:practice_mobile_app/navigation_demo.dart';
 import 'auth_screen.dart';
 
 void main() {
-  runApp(const ExpandedDemoScreen());
+  runApp(const NavigationDemo());
 }
