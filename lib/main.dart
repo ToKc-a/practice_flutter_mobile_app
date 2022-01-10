d7261ab302e8cb8d3749b3d611f105759e401a01
ToKc <ant.tokc@gmail.com>
2022-01-10 20:02:46 +0200

    add listview

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:practice_mobile_app/screens/expanded_demo_screen.dart';
+import 'package:practice_mobile_app/screens/listview_screen.dart';
 import 'package:practice_mobile_app/screens/navigation_demo.dart';
 import 'package:practice_mobile_app/screens/theme_demo_screen.dart';
 import 'screens/auth_screen.dart';
 
 void main() {
-  runApp(const ThemeDemoApp());
+  runApp(const ListViewScreen());
 }
