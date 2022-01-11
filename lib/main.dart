492fc5c130a3741b20af6be346f39632060ff02b
ToKc <ant.tokc@gmail.com>
2022-01-11 17:10:29 +0200

    add filesDemoScreen & shared prefs

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:practice_mobile_app/screens/expanded_demo_screen.dart';
+import 'package:practice_mobile_app/screens/file_demo_screen.dart';
 import 'package:practice_mobile_app/screens/listview_screen.dart';
 import 'package:practice_mobile_app/screens/navigation_demo.dart';
+import 'package:practice_mobile_app/screens/shared_pref_screen.dart';
 import 'package:practice_mobile_app/screens/theme_demo_screen.dart';
 import 'screens/auth_screen.dart';
 
 void main() {
-  runApp(const ListViewScreen());
+  runApp(const FilesDemoScreen());
 }
