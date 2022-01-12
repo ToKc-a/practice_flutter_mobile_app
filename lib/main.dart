fee77f25d1b532ba525495f67a5db64f127e3591
ToKc <ant.tokc@gmail.com>
2022-01-12 15:36:55 +0200

    add Networking Screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,11 @@ import 'package:practice_mobile_app/screens/expanded_demo_screen.dart';
 import 'package:practice_mobile_app/screens/file_demo_screen.dart';
 import 'package:practice_mobile_app/screens/listview_screen.dart';
 import 'package:practice_mobile_app/screens/navigation_demo.dart';
+import 'package:practice_mobile_app/screens/networking_screen.dart';
 import 'package:practice_mobile_app/screens/shared_pref_screen.dart';
 import 'package:practice_mobile_app/screens/theme_demo_screen.dart';
 import 'screens/auth_screen.dart';
 
 void main() {
-  runApp(const FilesDemoScreen());
+  runApp(const NetworkingScreen());
 }
